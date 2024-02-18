e82751a14de4eaa5c196db2bc20343a068d3c88f
shreyas-groww <shreyas.kadu@groww.in>
2024-02-18 09:34:35 +0530

    first-commit

---
+++ src/main/java/org/example/test/controller/TestController.java
@@ -0,0 +1,17 @@
+package org.example.test.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/test")
+public class TestController {
+
+  @GetMapping("/hello")
+  public ResponseEntity<String> getHello() {
+    return new ResponseEntity<>("hello", HttpStatus.OK);
+  }
+}
